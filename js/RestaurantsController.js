98a4b68d0a6072dcd3cf888acf9b426426256e6b
Ike Mattice & Jordan Johansen <student@epicodus.com>
2015-09-21 16:44:37 -0700

    Refactor search input and style.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,10 +1,11 @@
 Bestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope){
     $scope.restaurants = [{name: 'Montage', location: '123 Main St.', food_type: 'Cajun'}, {name: 'Portland City Grill', location: '30th floor of the Pink Building', food_type: 'New American Dishes with an Asian Touch'}];
     $scope.addRestaurant = function() {
-        $scope.restaurants.push({ name: $scope.name, location: $scope.location, food_type: $scope.foodType });
+        $scope.restaurants.push({ name: $scope.name, location: $scope.location, food_type: $scope.foodType, image: $scope.image });
         $scope.name = null;
         $scope.location = null;
         $scope.foodType = null;
+        // $scope.image = "'css/image_placeholder.png'";
     };
 
     $scope.deleteRestaurant = function(restaurant) {
